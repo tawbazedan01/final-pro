002a7c777f875884f9ce0270b4ff9511a0f7134b
tawbazedan01 <tawbazedan01@gmail.com>
2024-11-03 19:52:27 +0200

    edit-5

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,38 @@ function addToCart(e) {
 document.querySelectorAll(".add").forEach(function(button) {
     button.onclick = addToCart;
 });
+
+let firstNum = 0;
+
+function countPlus() {
+    if (firstNum <= 150) {
+        document.querySelector("#plus").textContent = firstNum;
+        firstNum++;
+    } else {
+        clearInterval(interval);
+    }
+}
+let count = 0;
+
+function countMillion() {
+    if (count <= 10) {
+        document.querySelector("#million").textContent = count;
+        count++;
+    } else {
+        clearInterval(interval);
+    }
+}
+
+let percentNum = 0;
+
+function countPercent() {
+    if (percentNum <= 100) {
+        document.querySelector("#percent").textContent = percentNum;
+        percentNum++;
+    } else {
+        clearInterval(interval);
+    }
+}
+let intervalPlus = setInterval(countPlus, 50);
+let intervalMillion = setInterval(countMillion, 300);
+let intervalPercent = setInterval(countPercent, 50);
