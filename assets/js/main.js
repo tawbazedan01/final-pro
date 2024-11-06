fd34988ec18f39e7684e1ea4c05b67955bf42c21
tawbazedan01 <tawbazedan01@gmail.com>
2024-11-06 18:19:07 +0200

    section-7

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,8 +15,6 @@ function countPlus() {
     if (firstNum <= 150) {
         document.querySelector("#plus").textContent = firstNum;
         firstNum++;
-    } else {
-        clearInterval(interval);
     }
 }
 let count = 0;
@@ -25,8 +23,6 @@ function countMillion() {
     if (count <= 10) {
         document.querySelector("#million").textContent = count;
         count++;
-    } else {
-        clearInterval(interval);
     }
 }
 
@@ -36,10 +32,21 @@ function countPercent() {
     if (percentNum <= 100) {
         document.querySelector("#percent").textContent = percentNum;
         percentNum++;
-    } else {
-        clearInterval(interval);
     }
 }
 let intervalPlus = setInterval(countPlus, 50);
 let intervalMillion = setInterval(countMillion, 300);
 let intervalPercent = setInterval(countPercent, 50);
+
+const countDown=()=>{
+  const now = new Date().getTime();
+  const hours = Math.floor((now%86400000)/(3600000));
+  const minutes = Math.floor((now%(1000*60*60))/(60000));
+  const seconds = Math.floor((now%(1000*60))/(1000));
+  document.querySelector(".hours").textContent=hours;
+  document.querySelector(".minutes").textContent=minutes;
+  document.querySelector(".seconds").textContent=seconds;
+}
+setInterval(()=>{
+  countDown();
+},1000)
